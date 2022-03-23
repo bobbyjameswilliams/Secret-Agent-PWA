8f220930a75a2ea9436dc7d1e4318431508cf7a5
Bobby Williams <bobby.james.williams@outlook.com>
2022-03-23 12:24:42 +0000

    axios installed

--- Main server/Assignment starting point/routes/card.js
+++ Main server/Assignment starting point/routes/card.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 class Card{
   title;
@@ -18,9 +19,24 @@ function getCards(){
   return [card1,card2];
 }
 /* GET users listing. */
+
 router.get('/', function(req, res, next) {
+  //#res.render('card', { title: 'Card View', cardList: getCards() });
+  console.log("deez nuts")
+  axios.post('http://localhost:3001/getArticles')
+      .then(json => res.json(json.data.result))
+      .catch(err => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(403).json(err)
+      })
+});
+
+router.post('/', function(req, res) {
+  console.log("deez nuts")
+  axios.post('http://localhost:3001/getArticles').then(json => res.json(json.data.result)).catch(console.log("This is a certified bruh moment"));
   res.render('card', { title: 'Card View', cardList: getCards() });
 });
+
 
 router.get('/room/',function (req, res) {
   let roomNo = req.query.roomNo;
