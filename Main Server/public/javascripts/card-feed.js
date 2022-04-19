d50ff5f186ba4be689699c7d0ebda917e8e0af18
Bobby Williams <bobby.james.williams@outlook.com>
2022-04-19 19:13:23 +0100

    localhost meant that remote users were being routed wrong

--- Main Server/public/javascripts/card-feed.js
+++ Main Server/public/javascripts/card-feed.js
@@ -1,5 +1,5 @@
 function connectToRoomPage(cardID){
     let roomNo = document.getElementById(cardID+'room_input').value;
     let username = document.getElementById(cardID+'username_input').value;
-    window.location.href = "http://localhost:3000/card/room/?roomNo="+cardID+roomNo+"&username="+username;
+    window.location.href = "/card/room/?roomNo="+cardID+roomNo+"&username="+username;
 }
