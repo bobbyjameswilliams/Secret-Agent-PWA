cdc89876f844f1349d7a05598ee449109063b81f
Daniel Whiteman <danielwhiteman718@gmail.com>
2022-04-20 22:20:13 +0100

    New caching strategy - may be changed

--- Main Server/app.js
+++ Main Server/app.js
@@ -12,7 +12,7 @@ var insertRouter = require('./routes/insert')
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
