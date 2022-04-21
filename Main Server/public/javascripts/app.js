e9c8a41c24bd92316559a64afe30e05090a3c59e
Bobby Williams <bobby.james.williams@outlook.com>
2022-04-21 14:13:04 +0100

    re-add deleted files

---
+++ Main Server/public/javascripts/app.js
@@ -0,0 +1,19 @@
+
+/**
+ * called by the HTML onload
+ * declaring the service worker
+ */
+
+function initBrowser(){
+    initServiceWorker();
+}
+
+function initServiceWorker() {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./service-worker.js')
+            .then(function () {
+                console.log('Service Worker Registered');
+            });
+    }
+}
