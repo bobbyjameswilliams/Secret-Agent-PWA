082f9f3e6dcbaaa087cfed1ad022b5eb5678bde0
Callum McCreadie <cmccreadie1@sheffield.ac.uk>
2022-04-21 17:00:36 +0100

    One Page Website

--- Main Server/app.js
+++ Main Server/app.js
@@ -5,9 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var cardRouter = require('./routes/card');
-var insertRouter = require('./routes/insert')
 
 var app = express();
 
@@ -22,10 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/card',cardRouter)
-app.use('/insert',insertRouter)
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
