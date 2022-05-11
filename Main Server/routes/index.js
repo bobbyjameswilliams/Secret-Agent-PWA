64b39e719299cbbe4cf936bf39f863a84ee56c37
Bobby Williams <bobby.james.williams@outlook.com>
2022-05-11 17:50:56 +0100

    Pass through routes to make calls to mongodb via main server, so browser can attempt to retrieve mongo data.

--- Main Server/routes/index.js
+++ Main Server/routes/index.js
@@ -26,16 +26,13 @@ class Card{
 router.get('/', function(req, res, next) {
     console.log("/ get called")
     axios.post('http://localhost:3001/getArticles',{}).then(json => {
-        // This could be a render? unsure if res.render is needed.
         var cards = [];
         json.data.forEach(function(article) {
           let card = new Card(article._id, article.title, article.file_path,
               article.description, article.author_name, article.date_of_issue);
           cards.push(card);
         });
-
-        //For now this is gonna be res.render while I figure out axios.
-        //TODO: look into this at a later date
+        //Renders homepage
         res.render('index', { title: 'Card View', cardList: cards });
     }).catch(err => {
         console.log("Error Rendering Index")
@@ -62,25 +59,35 @@ router.post('/', function(req, res) {
     })
 })
 
+//Passes data through to get articles from mongo.
 router.post('/getArticles', function(req, res, next) {
     console.log("/getAllArticles get called")
     axios.post('http://localhost:3001/getArticles',{}).then(json => {
-        var cards = [];
-        json.data.forEach(function(article) {
-            let card = new Card(article._id, article.title, article.file_path,
-                article.description, article.author_name, article.date_of_issue);
-            cards.push(card);
-        });
-
-        //For now this is gonna be res.render while I figure out axios.
-        //TODO: look into this at a later date
-        res.append(json.data)
+        res.send(json.data)
     }).catch(err => {
-        console.log("Error Rendering Index")
+        console.log("Error getting articles")
         res.setHeader('Content-Type', 'application/json');
         res.status(403).json(err)
     })
 });
 
+//Passes data through to insert article into mongo.
+router.post('/insertArticle', function(req, res) {
+    console.log("/insertArticle called")
+    axios.post('http://localhost:3001/insertArticle',{
+        title: req.body.title,
+        file_path: req.body.file_path,
+        description: req.body.description,
+        author_name: req.body.author_name,
+        date_of_issue: Date.now()
+    }).then(json => {
+        console.log(JSON.stringify(json.data));
+        res.send(json.data)
+    }).catch(err => {
+        console.log("Error")
+        res.setHeader('Content-Type', 'application/json');
+        res.status(403).json(err)
+    })
+})
 
 module.exports = router;
