1ff3a17eef1c869e0fa32136f0adaaa662f21ca7
Callum McCreadie <cmccreadie1@sheffield.ac.uk>
2022-05-13 18:42:03 +0100

    Create Article Card

--- Main Server/public/javascripts/card-feed.js
+++ Main Server/public/javascripts/card-feed.js
@@ -4,3 +4,77 @@ function connectToRoomPage(cardID, image){
     changeRoomView();
     initRoom(roomNo, username, image);
 }
+
+function createArticleCard(article){
+    let row = document.createElement('div');
+    row.className = 'row'
+    let col = document.createElement('col-sm');
+    let card = document.createElement('div');
+    card.className = 'card article-card';
+    card.id = article.id
+
+    let cardImg = document.createElement('img');
+    cardImg.id = article.id + 'img'
+    cardImg.class = 'card-img'
+    cardImg.alt = 'Card image cap'
+    cardImg.src = article.image
+
+    let cardBody = document.createElement('div')
+    cardBody.className = "card-body"
+
+    let cardTitle = document.createElement('h3');
+    cardTitle.className = 'card-title';
+    cardTitle.innerHTML = article.title;
+
+    let cardText = document.createElement('p');
+    cardText.className = 'card-text';
+    cardText.innerHTML = article.description;
+
+    let secondRow = document.createElement('div')
+    secondRow.className = 'row'
+    let col5 = document.createElement('div')
+    col5.className = 'col-5'
+    let col4 = document.createElement('div')
+    col4.className = 'col-4'
+    let col3 = document.createElement('div')
+    col3.className = 'col-3'
+    let usernameInput =  document.createElement('input');
+    usernameInput.className = 'text_input';
+    usernameInput.id = article.id + "username_input";
+    usernameInput.placeholder = "Enter Username"
+    usernameInput.type = 'text';
+
+    let roomInput =  document.createElement('input');
+    roomInput.className = 'text_input';
+    roomInput.id = article.id + "username_input";
+    roomInput.placeholder = "Enter Username"
+    roomInput.type = 'text';
+
+    let submit = document.createElement('button');
+    submit.className = 'btn btn-primary';
+    submit.id = "connect";
+    submit.placeholder = "Enter Username"
+    submit.innerHTML = 'Connect'
+    submit.addEventListener('click',function (){
+        let roomNo = document.getElementById(article.id+'room_input').value;
+        let username = document.getElementById(article.id+'username_input').value;
+        changeRoomView();
+        initRoom(roomNo, username, article.image);
+    })
+
+    row.appendChild(col);
+    col.appendChild(card);
+    card.appendChild(cardImg);
+    card.appendChild(cardBody);
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
+    cardBody.appendChild(secondRow);
+    secondRow.appendChild(col5);
+    col5.appendChild(usernameInput);
+    secondRow.appendChild(col4);
+    col4.appendChild(roomInput);
+    secondRow.appendChild(col3);
+    col3.appendChild(submit);
+
+    return card
+}
