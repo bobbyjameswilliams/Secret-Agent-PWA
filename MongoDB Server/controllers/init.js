d16004fd43921c4a8e52b6a194e9f9859543f812
Bobby Williams <bobby.james.williams@outlook.com>
2022-05-20 16:20:20 +0100

    Removed code that clears mongo.

--- MongoDB Server/controllers/init.js
+++ MongoDB Server/controllers/init.js
@@ -5,7 +5,7 @@ const Article = require('../models/articles');
 exports.init= function() {
     // uncomment if you need to drop the database
 
-    Article.remove({}, function(err) {
-        console.log('collection removed')
-    });
+    //Article.remove({}, function(err) {
+    //    console.log('collection removed')
+    //});
 }
